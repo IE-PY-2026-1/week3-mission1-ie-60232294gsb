a1b869d24012a5f568cc35895445d95088a1dbd7
60232294gsb <getspicybear@mju.ac.kr>
2026-03-17 23:07:36 +0900

    Update main.py

--- main.py
+++ main.py
@@ -1,13 +1,12 @@
-# 1. 사용자 입력
-name3 = input("이름을 입력하시오:")
+name3 = str(input("이름을 입력하시오:"))
 writing3 = int(input("당신의 글쓰기 점수를 입력하시오:"))
 python3 = int(input("당신의 파이썬 점수를 입력하시오:"))
 last_avg3 = float(input("당신의 지난학기 평균을 입력하시오:"))
 
 # 2. 계산
-average3 = (writing3 + python3) / 2
-diff3 = average3 - last_avg3
+average3 = (writing3 + python3)/2
+diff = average3 - last_avg3
 
 # 3. 출력
 print(f"{name3} 학생의 글쓰기 점수는 {writing3}, 파이썬 점수는 {python3} 입니다.")
-print(f"평균은 {average3} 이고, 지난 학기와의 차이는 {diff3} 입니다.")
+print(f"평균은 {average3} 이고, 지난 학기와의 차이는 {diff} 입니다.")
