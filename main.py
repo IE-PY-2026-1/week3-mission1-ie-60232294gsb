10e03edc7a6e56d2b4cf0d200981bd60d08c54b8
60232294gsb <getspicybear@mju.ac.kr>
2026-03-17 17:48:42 +0900

    Update main.py

--- main.py
+++ main.py
@@ -2,14 +2,15 @@
 # 작 성 자 : 신지웅
 # 미션 1
 # 1. 사용자 입력
-name2 = str(input("이름을 입력하시오:"))
-writing2 = int(input("당신의 글쓰기 점수를 입력하시오:"))
-python2 = int(input("당신의 파이썬 점수를 입력하시오:"))
-last_avg2 = float(input("당신의 지난학기 평균을 입력하시오:"))
+name3 = str(input("이름을 입력하시오:"))
+writing3 = int(input("당신의 글쓰기 점수를 입력하시오:"))
+python3 = int(input("당신의 파이썬 점수를 입력하시오:"))
+last_avg3 = float(input("당신의 지난학기 평균을 입력하시오:"))
+diff3 = average3 - last_avg3
 
 # 2. 계산
-average2 = (writing2 + python2)/2
+average3 = (writing3 + python3)/2
 
 # 3. 출력
-print(f"\n{name2} 학생의 글쓰기 점수는 {writing2}, 파이썬 점수는 {python2} 입니다.")
-print(f"평균은 {average2} 이고, 지난 학기와의 차이는 {average2 - last_avg2 } 입니다.")
+print(f"\n{name3} 학생의 글쓰기 점수는 {writing3}, 파이썬 점수는 {python3} 입니다.")
+print(f"평균은 {average3} 이고, 지난 학기와의 차이는 {diff3} 입니다.")
